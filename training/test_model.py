b8a0146fd64c273abd857db9a962180c90ab7aef
Giulio Perillo <79469450+sand-knight@users.noreply.github.com>
2023-03-29 01:47:13 +0200

    added some context for the test
    |         IA         | belt  | moved |        choi        |        choe        |
    | 68.80353546142578  | 59.0  |   1   | 76.05633802816901  | 67.52411575562701  |
    | 64.57345581054688  | 58.0  |   1   | 65.625             | 66.87898089171975  |

--- training/test_model.py
+++ training/test_model.py
@@ -1,19 +1,21 @@
 import numpy
 import tensorflow._api.v2.compat.v1 as tensorflow
 from models import deeper_fcn
+from analytical_methods import *
 
 aggregated = numpy.genfromtxt("../aggr_24-03-23.csv", delimiter=",")
 raw = numpy.genfromtxt("../newraw_24-03-23.csv", delimiter=",")
 
 input = []
 output = []
+movement_detected = []
 
 ################### CREATE DATA SET
 ### FOR EACH AGGREGATE DATA FETCH A 400 ARRAY OF RAW DATA
 ### HAVING THE HIGHEST TIMESTAMP LESS THAN THE AGGREGATE TIMESTAMP
 
 last_index = 0
-for tuple in aggregated:
+for aggrindex, tuple in enumerate(aggregated):
     if tuple[2] == 0:
         continue
     timestamp = tuple[0]
@@ -27,9 +29,20 @@ for tuple in aggregated:
     input.append(raw[last_index-400:last_index,1])
     output.append(tuple[2])
 
-input = numpy.expand_dims(input, axis=2)
+    #in the last 8 seconds, had the user moved
+    hasmoved = False
+    i = 0
+    while timestamp-aggregated[aggrindex-i][0] < 8:
+        if aggregated[aggrindex-i][4] > 0:
+            hasmoved = True
+        i+=1
+    movement_detected.append(hasmoved)
+
+
+
+tinput = numpy.expand_dims(input, axis=2)
 output = numpy.array(output)
-print("data shapes: ", input.shape, output.shape)
+print("data shapes: ", tinput.shape, output.shape)
 
 #tensorflow
 tensorflow.logging.set_verbosity(tensorflow.logging.ERROR)
@@ -42,7 +55,8 @@ with open("output/deeper_fcn1/model.json", "r") as fp:
 model = tensorflow.keras.models.model_from_json(json)
 model.load_weights("output/deeper_fcn1/weights")
 
-result = model.predict(input)
+result = model.predict(tinput)
 
-for i in range (1,500):
-    print("IA says", result[i][0],", belt says ",output[i])
+print ("| {:^18} | {:^5} | moved | {:^18} | {:^18} |".format("IA", "belt", "choi", "choe"))
+for i in range (1340,1400):
+    print("| {:<18} | {:<5} | {:^5} | {:<18} | {:<18} |".format(result[i][0],output[i],movement_detected[i],diff_mean_choi(input[i]), diff_mean_choe(input[i])))
